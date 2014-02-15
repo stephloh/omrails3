23454e9d5747a23444723e87d41ab0aa7c9d0247
Stephanie Loh <sloh@booster.com>
2014-02-15 10:15:22 +0800

    installed the right gem sqlite3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # This is the sample application for
 [*One Month Rails: Teach Yourself to Code*](http://onemonthrails.com)
+
+#By Mattan Grifflel of One Month Rails
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Omrails3::Application.routes.draw do
-  get "pages/home"
+get 'pages#home'
+#root :to => 'pages#home'
+#get 'about' => 'pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
